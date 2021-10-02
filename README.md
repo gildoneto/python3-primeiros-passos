d422628430b5a104f2c67e41b3579ffe7a9d0343
Avanade - Gildo Neto <avanade.gildo@grupofleury.com.br>
2021-10-02 11:47:35 -0300

    cria nova pasta

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Primeiros passos com o Python 3
 
+## Tipos primitivos
+
+- int -> números inteiros
+- float -> números reais
+- bool -> booleano [`True`, `False`]
+- str -> string -> caracteres
+
+## Formato de variáveis dentro de strings [Python 3]
+
+- Usar a função format()
+    - `('string qualquer {} com {} variáveis {}'.format(var0, var1, var2)`
+- Também é possível numerar as máscaras{}, sempre começando a contagem de 0
+    -  `('string qualquer {2} com {0} variáveis {1}'.format(var0, var1, var2)`
---
+++ sintaxe/001-tipos-primitivos-a.py
@@ -0,0 +1,24 @@
+#_| inputs são considerados strings
+# testeTipo01 = input('Digite um numero: ')
+# print(type(testeTipo01))
+
+#_| pra que ele seja armazenado com int
+#_| é necessário colocar o input dentro da funcao int()
+# testeTipo02 = int(input('Digite um numero:  '))
+# print(type(testeTipo02))
+
+#_| somando dois números com a função int
+# number01 = int(input('number 01: '))
+# number02 = int(input('number 02: '))
+# sum = number01 + number02
+# print('O total da soma é', sum)
+
+#_| somando dois números sem a função int
+#_| teremos uma concatenação ao invés de uma soma
+number03 = input('number 03: ')
+number04 = input('number 04: ')
+sum = number03 + number04
+# print('Neste caso você está concatenando os números', number03, 'e', number04, 'que vai resultar em:', sum)
+#_| existe uma forma melhor de adicionar variáveis dentro de uma string.
+#_| usando o a funcao format() e dentro da string usar a mascara {}
+print('Neste caso você está concatenando os números {} e {} que vai resultar em: {}'.format(number03, number04, sum))
