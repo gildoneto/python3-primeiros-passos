c448a8e001f9dc549902a22b6099e43fe37cb659
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 20:54:01 -0300

    Entrega exercício 006

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -1,1 +1,9 @@
 ## Crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada.
+
+valorDigitado = int(input('Digite um número: '))
+
+dobro = valorDigitado * 2
+triplo = valorDigitado * 3
+raiz = valorDigitado**(1/2)
+
+print('O dobro de {} é {}. O triplo é {}, e raiz quadrada é {}'.format(valorDigitado, dobro, triplo, raiz))
