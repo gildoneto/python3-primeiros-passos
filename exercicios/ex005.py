d355fd4435da6037929d10001d96aeff8ebad890
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 20:46:35 -0300

    Entrega exercício 005

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -1,1 +1,7 @@
  ## Faça um programa que leia um número inteiro e mostra na teela o seu sucessor e seu atencessor
+
+valorDigitado = int(input('Digite um número: '))
+antecessor = valorDigitado - 1
+sucessor = valorDigitado + 1
+
+print('O valor antecessor a {} é {} e o sucessor é {}'.format(valorDigitado, antecessor, sucessor))
