abecf55b263433d6dae0a5806dbc5c99334de126
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 22:02:50 -0300

    Entrega exercício 010

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,3 +1,9 @@
 ## Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
 ## Considere
 ## US$1,00 = R$3,27
+
+real = float(input('Digite um valor em reais: R$'))
+
+dolar = real / 3.27
+
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
