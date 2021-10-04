cc509b08d6f97d4b258ccc4f6fb2a6344e8b1629
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 21:00:41 -0300

    Entrega exercício 007

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -1,1 +1,8 @@
 ## Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+media = (n1 + n2)/2
+
+print('A média entre as notas {} e {} é {}'.format(n1, n2, media))
