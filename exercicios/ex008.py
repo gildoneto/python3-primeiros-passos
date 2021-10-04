dac1e54b44f69fd9d6292dde81e3507064869686
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 21:37:45 -0300

    Entrega exercício 008

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,1 +1,8 @@
 ## Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+valorDigitado = int(input('Digite um valor: '))
+
+cent = valorDigitado * 100
+mili = valorDigitado * 1000
+
+print('{} metro(s) é igual a {} centímetros ou a {} milímetros.'.format(valorDigitado, cent, mili))
