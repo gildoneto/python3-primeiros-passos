97fd38f2308b47a93a6629258487c8da5833be77
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 21:49:32 -0300

    Entrega exercício 009

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -1,1 +1,16 @@
 ## Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+valor = int(input('Digite um número: '))
+
+print('Tabuada de {}'.format(valor))
+
+print('{} x 1 = {}'.format(valor, valor * 1))
+print('{} x 2 = {}'.format(valor, valor * 2))
+print('{} x 3 = {}'.format(valor, valor * 3))
+print('{} x 4 = {}'.format(valor, valor * 4))
+print('{} x 5 = {}'.format(valor, valor * 5))
+print('{} x 6 = {}'.format(valor, valor * 6))
+print('{} x 7 = {}'.format(valor, valor * 7))
+print('{} x 8 = {}'.format(valor, valor * 8))
+print('{} x 9 = {}'.format(valor, valor * 9))
+print('{} x 10 = {}'.format(valor, valor * 10))
