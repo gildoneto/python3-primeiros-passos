37c8bd6e8aae8dc9a7b570e9d6120babbffd2e66
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 22:15:28 -0300

    Entrega exercício 012

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,2 +1,7 @@
 ## Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
+produto = float(input('Digite o valor do produto em reais: R$'))
+
+desconto = produto - (produto * 0.05)
+
+print('O produto de valor R${:.2f} após o desconto de 5% passa a ser R${:.2f}'.format(produto, desconto))
