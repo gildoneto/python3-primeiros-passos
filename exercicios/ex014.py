6f3377a18a4ccfaec105e8a4bb5907befd36c094
Gildo Neto <gildoneto@hotmail.com>
2021-10-03 23:49:25 -0300

    Refatora exercicio 014

--- exercicios/ex014.py
+++ exercicios/ex014.py
@@ -1,6 +1,7 @@
 ## Conversor de Temperaturas
 
 celsius = float(input('Digite a temperatura em C: '))
-fahrenheit = (celsius * 9/5) + 32
+#fahrenheit = (celsius * 9/5) + 32
+fahrenheit = 9 * celsius / 5 + 32
 
 print('{:.2f} graus Celsius equivale a {:.2f} graus em Fahrenheit'.format(celsius, fahrenheit))
