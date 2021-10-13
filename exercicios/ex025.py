7f38523ef9b23b97b0bb3a845e3284c665c4d37c
Gildo Neto <gildoneto@hotmail.com>
2021-10-13 20:47:57 -0300

    Entrega exercicio 25

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,1 +1,11 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+nomeCompleto = input('Digite seu nome completo: ').upper()
+
+posicaoSilva = nomeCompleto.find('SILVA')
+tamSilva = posicaoSilva + 5
+
+if posicaoSilva >= 0:
+    print('NOME TEM', nomeCompleto[posicaoSilva: tamSilva])
+else:
+    print('NOME NÃO TEM SILVA')
