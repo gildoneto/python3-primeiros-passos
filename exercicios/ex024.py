fe4b99fa20deb06d04a4d9558b2258b6c6e4d9cb
Gildo Neto <gildoneto@hotmail.com>
2021-10-13 20:26:30 -0300

    Entrega exercicio 24

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,1 +1,11 @@
-# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+# Crie um programa que leia o nome de uma cidade e diga 
+# se ela começa ou não com o nome "SANTO".
+
+cidade = input('Digite o nome da sua cidade: ')
+
+splitCidade = cidade.split()[0].upper()
+
+if splitCidade == 'SANTO':
+    print('Começa com a palavra', splitCidade)
+else:
+    print('NÃO começa com a palavra SANTO')
