5505025134eb0e7c4f412964e3c81ced30eda7ad
Gildo Neto <gildoneto@hotmail.com>
2021-10-13 09:53:45 -0300

    Adiciona exercicios 22 à 27

---
+++ sintaxe/004-manipulando-texto-a.py
@@ -0,0 +1,20 @@
+frase = 'Curso em Vídeo Python'
+
+print(frase[3])
+print(frase.count('O'))
+print(frase.upper().count('O'))
+
+print(len(frase))
+print(len(frase.strip()))
+
+## imprimindo na tela um texto extenso => 3 aspas duplas
+
+# print("""
+# Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+# Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+# when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+# It has survived not only five centuries, but also the leap into electronic typesetting, 
+# remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+# sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+# Aldus PageMaker including versions of Lorem Ipsum.
+# """)
