f906a8f199c0ba2abfb3d9aef98ba915b9a9ab38
Gildo Neto <gildoneto@hotmail.com>
2021-10-13 21:00:46 -0300

    Entrega exercicio 26

--- exercicios/ex026.py
+++ exercicios/ex026.py
@@ -1,8 +1,8 @@
 # Faça um programa que leia uma frase pelo teclado e mostre:
-
+frase = input('Formule uma frase: ')
 # Quantas vezes aparece a letra "A"
-
+print('A letra "A" aparece',frase.count('A'), 'vezes')
 # Em que posição ela aparece a primeira vez
-
+print('Primeira posicao palavra "A":', frase.find('A'))
 # Em que posição ela aparece a última vez.
-
+print('Ultima posicao palavra "A":', frase.rfind('A'))
