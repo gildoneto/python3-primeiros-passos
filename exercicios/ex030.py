1a966bf00bc177c6de984ab8f8bc646de64ba5c8
Gildo Neto <gildoneto@hotmail.com>
2021-10-15 19:53:45 -0300

    Entrega exercicio 30

--- exercicios/ex030.py
+++ exercicios/ex030.py
@@ -1,2 +1,9 @@
 # Crie um programa que leia um número inteiro e mostre na tela
 # se é PAR ou ÍMPAR.
+
+num = int(input('Digite um numero: '))
+
+if num % 2 == 0:
+    print('O número {} é PAR'.format(num))
+else:
+    print('O número {} é ÍMPAR'.format(num))
