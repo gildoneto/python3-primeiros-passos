18e852b4d90509927a4593937ace4984c9ace56d
Gildo Neto <gildoneto@hotmail.com>
2021-10-15 19:47:28 -0300

    Entrega exercicio 29

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -3,3 +3,10 @@
 # Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
 
 # A multa vai custar R$7,00 por cada Km acima do limite.
+
+multa = 7.0
+
+Km = int(input('Velocidade em Km: '))
+
+if Km > 80:
+    print('Você ultrapassou o limite, sua multa é de R${:.2f}'.format((Km - 80) * multa))
