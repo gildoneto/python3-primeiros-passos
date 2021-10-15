8e2503e45831362ec5547aaa5bf5bc3ba4eaa3f2
Gildo Neto <gildoneto@hotmail.com>
2021-10-15 20:40:43 -0300

    Entrega exercicio 32

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,1 +1,8 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+
+ano = int(input('Digite o ano: '))
+
+if ano % 100 != 0 and ano % 4 == 0 or ano % 400 == 0:
+    print('É um ano bissexto')
+else:
+    print('Não é bissexto')
