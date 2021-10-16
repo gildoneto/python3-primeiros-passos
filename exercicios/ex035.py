9a4ea636f42492ccdfb93ca08897f5337fc6c278
Gildo Neto <gildoneto@hotmail.com>
2021-10-15 21:42:53 -0300

    Entrega exercicio 35

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -1,2 +1,13 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga
 # ao usuário se elas podem ou não formar um triângulo.
+
+# A soma das duas menores retas precisa ser maior que a maior reta
+
+r1 = float(input('Digite a reta 1: '))
+r2 = float(input('Digite a reta 2: '))
+r3 = float(input('Digite a reta 3: '))
+
+if r1 + r2 > r3 and r2 + r3 > r1 and r3 + r1 > r2:
+    print('Pode formar um triangulo')
+else:
+    print('NAO Pode formar um triangulo')
