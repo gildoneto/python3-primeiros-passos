98281967d2ef73a51a909d771788da22ce9871c0
Gildo Neto <gildoneto@hotmail.com>
2021-10-15 21:28:46 -0300

    Entrega exercicio 34

--- exercicios/ex034.py
+++ exercicios/ex034.py
@@ -4,3 +4,10 @@
 # Para salários superiores a R$1.250,00, calcule um aumento de 10%.
 
 # Para os inferiores ou iguais, o aumento é de 15%.
+
+salario = float(input('Digite seu Salario: '))
+
+if salario <= 1250:
+    print('Seu salario de R${:.2f} aumentara 15%. Novo salario: R${:.2f}'.format(salario, salario + salario * 0.15))
+else:
+    print('Seu salario de R${:.2f} aumentara 10%. Novo salario: R${:.2f}'.format(salario, salario + salario * 0.10))
